e1f6f7ccb42079268e937187beec484799657ce4
Unity喵 <443044187@qq.com>
2020-04-16 22:42:42 +0800

    1.事件中心新增数据传递

---
+++ Assets/Scripts/CSharp/Game/InputController.cs
@@ -0,0 +1,19 @@
+﻿#region --------------------------文件信息--------------------------------------
+/******************************************************************
+** 文件名:	InputController
+** 版  权:	(C)  
+** 创建人:  Unity喵
+** 日  期:	
+** 描  述: 	
+*******************************************************************/
+#endregion
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LoveYouForever
+{
+	public class InputController : MonoBehaviour
+	{
+	}
+}
